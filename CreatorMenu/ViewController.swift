3b3a22a3444f01c1a913feb0bf05c3cda416af7b
Darragh Rogan <darragh@rogan.ie>
2020-06-07 10:13:04 +0100

    Initial Commit

---
+++ CreatorMenu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CreatorMenu
+//
+//  Created by Darragh Rogan on 07/06/2020.
+//  Copyright © 2020 Darragh Rogan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
