7a5999e18fa5afa289885e42235010cae6acad1f
Darragh Rogan <darragh@rogan.ie>
2020-06-28 11:13:21 +0100

    introduced images

---
+++ Per mille Widget/Shared/Per_mille_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Per_mille_WidgetApp.swift
+//  Shared
+//
+//  Created by Darragh Rogan on 28/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct Per_mille_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
